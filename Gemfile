94444dcbead7dd7275f8c63b50f86cd12a825523
Elle Meredith <ellemeredith@gmail.com>
2011-05-22 14:32:41 +1000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+gem 'sqlite3'
+
+gem 'capistrano'
+gem 'haml'
+gem 'hirb'
+gem 'inherited_resources'
+gem 'simple_form'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+group :development, :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'rspec-rails'
+  gem 'spork'
+  gem 'steak'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideagora::Application.config.secret_token = '19569987d18269e35a649acb317012d2cb91bac78b9e22186974b9a3c76938df148e0918c0ac0577aab8b6631caa6f9c747e693ae4059474d9194a85024520ab'
