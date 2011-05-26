a819a0f1f3bf41b4dbed96cb0d81da2df4f3de20
Gabe <gabe@avantbard.com>
2011-05-26 10:32:17 +1000

    change autotest discover to look for acceptance directory instead of integration

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -2,16 +2,16 @@ Autotest.add_discovery { "rails" }
 Autotest.add_discovery { "rspec2" }
 
 Autotest.add_hook :initialize do |at|
-  at.add_mapping(%r%^spec/integration/.*_spec.rb$%, true) { |filename, _|
+  at.add_mapping(%r%^spec/acceptance/.*_spec.rb$%, true) { |filename, _|
     filename
   }
 
   at.add_mapping(%r%^app/(models|controllers|helpers|lib)/.*rb$%, true) {
-    at.files_matching %r%^spec/integration/.*_spec.rb$%
+    at.files_matching %r%^spec/acceptance/.*_spec.rb$%
   }
 
   at.add_mapping(%r%^app/views/(.*)$%, true) {
-    at.files_matching %r%^spec/integration/.*_spec.rb$%
+    at.files_matching %r%^spec/acceptance/.*_spec.rb$%
   }
 end
 
