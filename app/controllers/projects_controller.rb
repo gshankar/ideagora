4d096449a53461702b7d0876ac97c2c4b5263cb9
Elle Meredith <ellemeredith@gmail.com>
2011-05-31 20:02:00 +1000

    user projects acceptance tests [wip]

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,25 @@
 class ProjectsController < InheritedResources::Base
-  before_filter :requires_login
-  before_filter :set_owner, :only => [:create, :update]
+  before_filter :requires_login, :except => [:index, :show]
+  before_filter :requires_owner, :except => [:index, :show, :new, :create]
   
   def create
+    @project = Project.new(params[:project])
+    @project.owner = current_user
     create! { user_path(current_user) }
   end
   
 private
-  def set_owner
-    @project = Project.new(params[:project])
-    @project.owner = current_user
+  def requires_owner
+    unless is_owner?
+      redirect_to :back
+      flash[:alert] = 'You cannot edit projects that are not yours'
+    end
   end
+  
+  def is_owner?
+    resource.owner == current_user
+  end
+  helper_method :is_owner?
   
   def collection
     @projects ||= if current_user
--- spec/acceptance/user_projects_spec.rb
+++ spec/acceptance/user_projects_spec.rb
@@ -2,47 +2,97 @@ require 'acceptance/acceptance_helper'
 
 describe 'User\'s projects', :type => :request do
   before do
+    clear_db
     @c = Camp.make!
     @u = @c.users.make!
-    sign_in_as(@u)
+    @p = Project.make!(:owner => @u)
+  end
+  
+  context 'when not logged in' do
+    it 'can see /projects' do
+      pending
+      visit projects_path
+      page.should have_content(@p.name)
+    end
+    
+    it 'can see /users/1/projects' do
+      pending
+      visit user_projects_path(@u)
+      page.should have_content(@p.name)
+    end
+    
+    it 'cannot access /users/1/projects/new' do
+      visit new_user_project_path(@u)
+      assert_unauthorised
+    end
   end
 
   context 'when logged in' do
-    it "should not create a project without a name" do
-      pending
+    before do
+      sign_in_as(@u)
     end
-    
-    
-    it 'creates project with owner as current_user' do
-      visit user_path(@u)
-      page.should have_content('Add new project')
-      click_link 'Add new project'
+
+    context 'when new project' do
+      it "should not create a project without a name" do
+        visit user_path(@u)
+        page.should have_content('Add new project')
+        click_link 'Add new project'
       
-      page.should have_content('Create a new project')
-      
-      attrs = { 
-        :name => 'Happy campers',
-        :description => 'App for railscamps',
-      }
-      
-      attrs.each do |attr, value|
-        fill_in attr.to_s.humanize, :with => value
+        page.should have_content('Create a new project')
+        
+        page.click_button 'Create Project'
+        
+        current_path.should == user_projects_path(@u)
+        page.should have_content "can't be blank"
       end
       
-      page.click_button 'Create Project'
+      it 'creates and sets current_user as owner' do
+        visit new_user_project_path(@u)
+        
+        attrs = { 
+          :name => 'Happy campers',
+          :description => 'App for railscamps',
+        }
       
-      current_path.should == user_path(@u)
-      page.should have_content('Happy campers')
+        attrs.each do |attr, value|
+          fill_in attr.to_s.humanize, :with => value
+        end
+      
+        page.click_button 'Create Project'
+      
+        current_path.should == user_path(@u)
+        page.should have_content('Happy campers')
 
-      # check flash
-      # assert that a new project was created
-      # assert tha current_user.projects.count changed by 1
+        # check flash
+        @u.projects.count.should == 2
+        Project.last.owner == @u
+      end
     end
+    
+    context 'with existing project' do
+      it "cannot edit else's project" do
+        @other = User.make!
+        @other_project = Project.make!(:owner => @other)
+        visit root_url
+        visit edit_user_project_path(@other, @other_project)
+        
+        current_path.should == root_path
+        page.should have_content('You cannot edit projects that are not yours')
+      end
+      
+      it "can edit own projects" do
+        pending
+        visit user_path(@u)
+        
+      end
+      
+      it "can delete own project" do
+        pending
+        visit user_path(@u)
+      end
+    end
+    
+    
   end
 
-  context 'when not logged in' do
-    it 'can see /projects'
-    it 'can see /users/1/projects'
-    it 'cannot access /users/1/projects/new'
-  end
 end
